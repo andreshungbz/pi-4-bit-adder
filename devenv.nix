79a996f198998009f7f4d3198de2392690fc9116
Andres Hung <2018118240@ub.edu.bz>
2026-09-21 05:21:39 +0000

    feat: set up development environment

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{
+  pkgs,
+  ...
+}:
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    git
+    just
+    just-lsp
+  ];
+
+  # https://devenv.sh/languages/
+  languages = {
+    python = {
+      enable = true;
+      venv.enable = true;
+      uv = {
+        enable = true;
+        sync.enable = true;
+      };
+    };
+  };
+
+  # https://devenv.sh/scripts/
+  scripts.version.exec = ''
+    python3 --version
+    uv --version
+  '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    version
+  '';
+}
